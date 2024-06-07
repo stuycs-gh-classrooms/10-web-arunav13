c4fa69415b90a9cba477bb985f5c5ff637188c1e
arunav13 <avaithilingam60@stuy.edu>
2024-06-06 23:47:52 -0400

    c

--- final_project/dynaform.py
+++ final_project/dynaform.py
@@ -82,7 +82,7 @@ if (len(data) == 0):
 else:
     question_num = data['question'].value
     choice_num = data['choice'].value
-    if question_num in ['1bba' , '1aabba' , '1bba' , '1aabaa' '1aabbc', '1aabbb' , '1bbb' '1ba' ,'1aaa' , '1aba'] and choice_num == 'a':
+    if question_num in ['1bba' , '1bbc' '1aabba' , '1bba' , '1aaba' '1aabbc', '1aabbb' , '1bbb' '1ba' ,'1aaa' , '1aba'] and choice_num == 'a':
         question_num = '1'
     else:
         question_num += choice_num
